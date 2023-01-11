f716cd3b8ea547221a66f24e89046f09c1de49a6
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2023-01-11 13:34:03 +0500

    Initial commit

---
+++ app/src/main/java/com/joerakhimov/niceweatherdi/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.joerakhimov.niceweatherdi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}

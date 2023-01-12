3287dad4b84daac53a407de5aa3ae001a82f6577
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2023-01-12 14:50:44 +0500

    dependency injection with hilt

---
+++ app/src/main/java/com/joerakhimov/niceweatherdi/App.kt
@@ -0,0 +1,9 @@
+package com.joerakhimov.niceweatherdi
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application(){
+
+}
--- app/src/main/java/com/joerakhimov/niceweatherdi/forecast/ForecastActivity.kt
+++ app/src/main/java/com/joerakhimov/niceweatherdi/forecast/ForecastActivity.kt
@@ -5,21 +5,18 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.joerakhimov.niceweatherdi.R
 import com.joerakhimov.niceweatherdi.data.Api
-import com.joerakhimov.niceweatherdi.data.BASE_URL
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_forecast.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ForecastActivity : AppCompatActivity() {
 
-    private val api =  Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(Api::class.java)
+    @Inject
+    lateinit var api: Api
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

6d93373f127ca8aed1b2e21fdb23218bacc3975f
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2023-01-13 17:20:29 +0500

    HTTP Interceptor

--- app/src/main/java/com/joerakhimov/niceweatherdi/di/AppModule.kt
+++ app/src/main/java/com/joerakhimov/niceweatherdi/di/AppModule.kt
@@ -1,5 +1,7 @@
 package com.joerakhimov.niceweatherdi.di
 
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.joerakhimov.niceweatherdi.data.Api
 import com.joerakhimov.niceweatherdi.data.BASE_URL
 import com.joerakhimov.niceweatherdi.data.RemoteRepository
@@ -8,7 +10,10 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -18,14 +23,24 @@ import javax.inject.Singleton
 class AppModule {
 
     @Provides
+    fun provideHttpInterceptor(@ApplicationContext context: Context): Interceptor =
+        ChuckerInterceptor.Builder(context).build()
+
+    @Provides
+    fun provideOkHttpClient(interceptor: Interceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+    @Provides
     @Singleton
-    fun provideApi(): Api {
-        return Retrofit.Builder()
+    fun provideApi(client: OkHttpClient): Api =
+        Retrofit.Builder()
+            .client(client)
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(Api::class.java)
-    }
 
 //    @Provides
 //    fun provideRepository(api: Api): Repository = RemoteRepository(api)

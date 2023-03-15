84dd323a53bb6d69166c13cf8b1ab7672294546f
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2023-03-15 15:42:34 +0500

    Get weather forecast without block main thread

--- app/src/main/java/com/joerakhimov/niceweatherdi/forecast/ForecastActivity.kt
+++ app/src/main/java/com/joerakhimov/niceweatherdi/forecast/ForecastActivity.kt
@@ -7,15 +7,13 @@ import com.joerakhimov.niceweatherdi.R
 import com.joerakhimov.niceweatherdi.data.Api
 import com.joerakhimov.niceweatherdi.data.BASE_URL
 import kotlinx.android.synthetic.main.activity_forecast.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ForecastActivity : AppCompatActivity() {
 
-    private val api =  Retrofit.Builder()
+    private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
@@ -27,18 +25,19 @@ class ForecastActivity : AppCompatActivity() {
         getForecast()
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
     private fun getForecast() {
-        runBlocking {
-            withContext(Dispatchers.IO) {
-                val forecast = api.getForecast()
-                forecast.daily?.let { showForecast(forecast) }
+        GlobalScope.launch {
+            val forecast = api.getForecast()
+            withContext(Dispatchers.Main) {
+                showForecast(forecast)
             }
         }
     }
 
     private fun showForecast(forecast: ForecastResponse) {
         title = forecast.location?.name
-        if(forecast.daily!=null){
+        if (forecast.daily != null) {
             recycler_forecast.layoutManager = LinearLayoutManager(this)
             recycler_forecast.adapter = ForecastAdapter(forecast.daily)
         }
